b0ad5c0db0a8e4830f99c77b384d0413bb8a63f7
sprajapati123 <sandisprajapati52@gmail.com>
2025-04-22 10:36:12 +0545

    insta clone

--- app/src/main/java/com/example/c36a/LoginActivity.kt
+++ app/src/main/java/com/example/c36a/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -15,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material3.Icon
@@ -23,7 +25,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
@@ -92,6 +96,18 @@ fun InstaClone(innerPadding: PaddingValues) {
                     .width(30.dp)
             )
         }
+
+        Row {
+            Image(
+                painter = painterResource(R.drawable.person),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.
+                    height(120.dp).width(120.dp).
+                    clip(shape = RoundedCornerShape(100.dp))
+            )
+        }
+
     }
 }
 
