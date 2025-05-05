671f7313b70ebe92dc2e19ece44164a862bb250f
sprajapati123 <sandisprajapati52@gmail.com>
2025-05-05 12:28:00 +0545

    login

---
+++ app/src/main/java/com/example/c36a/LoginActivity.kt
@@ -0,0 +1,46 @@
+package com.example.c36a
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.c36a.ui.theme.C36ATheme
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Scaffold { padding ->
+                LoginBody(padding)
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginBody(paddingValues: PaddingValues) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.White)) {
+
+    }
+}
+
+@Preview
+@Composable
+fun PreviewLogin(){
+    LoginBody(paddingValues = PaddingValues(0.dp))
+}
