2b137a46bdde0f7b5f6d5785545908f15db15db5
sprajapati123 <sandisprajapati52@gmail.com>
2025-05-05 12:59:00 +0545

    login

--- app/src/main/java/com/example/c36a/LoginActivity.kt
+++ app/src/main/java/com/example/c36a/LoginActivity.kt
@@ -9,14 +9,29 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.c36a.ui.theme.C36ATheme
@@ -35,15 +50,50 @@ class LoginActivity : ComponentActivity() {
 
 @Composable
 fun LoginBody(paddingValues: PaddingValues) {
+//    var counter : Int = 0
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
     Column(modifier = Modifier
         .fillMaxSize()
         .background(color = Color.White),
         horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
+
+
         Image(
             painter = painterResource(R.drawable.img),
             contentDescription = null
         )
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = {
+                email = it
+            },
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp),
+            placeholder = {
+                Text(text = "Enter email")
+            },
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.Gray.copy(alpha = 0.2f),
+                focusedIndicatorColor = Color.Green,
+                unfocusedContainerColor = Color.Gray.copy(alpha = 0.2f),
+                unfocusedIndicatorColor = Color.Blue
+            ),
+            shape = RoundedCornerShape(12.dp),
+            prefix = {
+                Icon(Icons.Default.Email, contentDescription = null)
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email
+            )
+        )
+
+
+
 
     }
 }
