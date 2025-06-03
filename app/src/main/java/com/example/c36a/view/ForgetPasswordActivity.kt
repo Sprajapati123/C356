a3879537f9b411872da2beb180b1e0eb5cf020ed
sprajapati123 <sandisprajapati52@gmail.com>
2025-06-03 09:33:22 +0545

    forget password

--- app/src/main/java/com/example/c36a/view/ForgetPasswordActivity.kt
+++ app/src/main/java/com/example/c36a/view/ForgetPasswordActivity.kt
@@ -6,11 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class ForgetPasswordActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,13 +35,32 @@ class ForgetPasswordActivity : ComponentActivity() {
 
 @Composable
 fun forgetBody() {
+    var email by remember { mutableStateOf("") }
     Scaffold  { padding->
         Column(
             modifier = Modifier
                 .padding(padding)
                 .fillMaxSize()
         ) {
+            OutlinedTextField(
+                value = email,
+                onValueChange = {
+                    email = it
+                },
+                placeholder = {
+                    Text("abc@gmail.com")
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
 
+            Button(
+                onClick = {
+
+                },
+                modifier = Modifier.fillMaxWidth().padding(top = 15.dp)
+            ) {
+                Text("Submit")
+            }
         }
     }
 }
