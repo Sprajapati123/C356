13c53261b79cb41d12650db5e23123767d407135
sprajapati123 <sandisprajapati52@gmail.com>
2025-06-06 11:02:10 +0545

    add product

--- app/src/main/java/com/example/c36a/view/DashboardActivity.kt
+++ app/src/main/java/com/example/c36a/view/DashboardActivity.kt
@@ -1,39 +1,58 @@
 package com.example.c36a.view
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 
 class DashboardActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-           DashboardBody()
+            DashboardBody()
         }
     }
 }
 
 @Composable
-fun DashboardBody(){
+fun DashboardBody() {
     val context = LocalContext.current
-    val activity = context as Activity
+    val activity = context as? Activity
 
-    val email : String? = activity.intent.getStringExtra("email")
 
-    Scaffold {
-        innerPadding->
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                val intent = Intent(context, AddProductActivity::class.java)
+                context.startActivity(intent)
+            }) {
+                Icon(Icons.Default.Add, contentDescription = null)
+            }
+        }
+    ) { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
-            Text("Good morning, $email")
+
         }
     }
+}
+
+@Preview
+@Composable
+fun preDash() {
+    DashboardBody()
 }
